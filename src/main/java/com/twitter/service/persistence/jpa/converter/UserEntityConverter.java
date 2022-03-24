6c3e54484d60f649bf915de8bcb1643baa521b11
wwf9w0 <aliemrekaradag1@gmail.com>
2022-03-24 13:55:10 +0300

    yml rfc

--- src/main/java/com/twitter/service/persistence/jpa/converter/UserEntityConverter.java
+++ src/main/java/com/twitter/service/persistence/jpa/converter/UserEntityConverter.java
@@ -3,11 +3,13 @@ package com.twitter.service.persistence.jpa.converter;
 import com.twitter.service.persistence.jpa.dto.UserDto;
 import com.twitter.service.persistence.jpa.entity.UserEntity;
 import com.twitter.service.persistence.jpa.request.UserRequest;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
+@Component
 public class UserEntityConverter {
 
     public UserDto toDto(UserEntity userEntity){
