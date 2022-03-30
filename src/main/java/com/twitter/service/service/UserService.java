bf3b29d5cdec46af2bc1e5e9ba790ee3d230fed6
wwf9w0 <aliemrekaradag1@gmail.com>
2022-03-30 19:55:18 +0300

    refactor

--- src/main/java/com/twitter/service/service/UserService.java
+++ src/main/java/com/twitter/service/service/UserService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
